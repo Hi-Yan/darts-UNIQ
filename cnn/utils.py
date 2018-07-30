9fb4f7385d926d182d825adf4d46f00a1103ac00
yochaiz <yochaizur@gmail.com>
2018-07-30 09:23:02 +0300

    * moved here functions from train_search

--- cnn/utils.py
+++ cnn/utils.py
@@ -1,40 +1,42 @@
 import os
 import numpy as np
 import torch
-import shutil
+from shutil import copyfile
+import logging
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+from torch import save as saveModel
 
 
 class AvgrageMeter(object):
 
-  def __init__(self):
-    self.reset()
+    def __init__(self):
+        self.reset()
 
-  def reset(self):
-    self.avg = 0
-    self.sum = 0
-    self.cnt = 0
+    def reset(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
 
-  def update(self, val, n=1):
-    self.sum += val * n
-    self.cnt += n
-    self.avg = self.sum / self.cnt
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
 
 
 def accuracy(output, target, topk=(1,)):
-  maxk = max(topk)
-  batch_size = target.size(0)
+    maxk = max(topk)
+    batch_size = target.size(0)
 
-  _, pred = output.topk(maxk, 1, True, True)
-  pred = pred.t()
-  correct = pred.eq(target.view(1, -1).expand_as(pred))
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-  res = []
-  for k in topk:
-    correct_k = correct[:k].view(-1).float().sum(0)
-    res.append(correct_k.mul_(100.0/batch_size))
-  return res
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 
 class Cutout(object):
@@ -60,61 +62,122 @@ class Cutout(object):
 
 
 def _data_transforms_cifar10(args):
-  CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
-  CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+    CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
 
-  train_transform = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
-  ])
-  if args.cutout:
-    train_transform.transforms.append(Cutout(args.cutout_length))
-
-  valid_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
     ])
-  return train_transform, valid_transform
+    if args.cutout:
+        train_transform.transforms.append(Cutout(args.cutout_length))
+
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+    return train_transform, valid_transform
 
 
 def count_parameters_in_MB(model):
-  return np.sum(np.prod(v.size()) for v in model.parameters())/1e6
-
-
-def save_checkpoint(state, is_best, save):
-  filename = os.path.join(save, 'checkpoint.pth.tar')
-  torch.save(state, filename)
-  if is_best:
-    best_filename = os.path.join(save, 'model_best.pth.tar')
-    shutil.copyfile(filename, best_filename)
+    return np.sum(np.prod(v.size()) for v in model.parameters()) / 1e6
 
 
 def save(model, model_path):
-  torch.save(model.state_dict(), model_path)
+    torch.save(model.state_dict(), model_path)
 
 
 def load(model, model_path):
-  model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path))
 
 
 def drop_path(x, drop_prob):
-  if drop_prob > 0.:
-    keep_prob = 1.-drop_prob
-    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
-    x.div_(keep_prob)
-    x.mul_(mask)
-  return x
+    if drop_prob > 0.:
+        keep_prob = 1. - drop_prob
+        mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+        x.div_(keep_prob)
+        x.mul_(mask)
+    return x
 
 
 def create_exp_dir(path, scripts_to_save=None):
-  if not os.path.exists(path):
-    os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-  if scripts_to_save is not None:
-    os.makedirs(os.path.join(path, 'scripts'))
-    for script in scripts_to_save:
-      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
-      shutil.copyfile(script, dst_file)
+    if scripts_to_save is not None:
+        os.makedirs(os.path.join(path, 'scripts'))
+        for script in scripts_to_save:
+            dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+            copyfile(script, dst_file)
 
+
+def save_state(state, is_best, path='.', filename='model'):
+    fileType = 'pth.tar'
+    default_filename = '{}/{}_checkpoint.{}'.format(path, filename, fileType)
+    saveModel(state, default_filename)
+    if is_best:
+        copyfile(default_filename, '{}/{}_opt.{}'.format(path, filename, fileType))
+
+
+def save_checkpoint(path, model, epoch, is_best=False):
+    state = dict(epoch=epoch + 1, state_dict=model.state_dict(), alphas=model.arch_parameters(), best_prec1=best_prec1)
+    save_state(state, is_best, path=path)
+
+
+def setup_logging(log_file, logger_name, propagate=False):
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    # logging to stdout
+    logger.propagate = propagate
+
+    return logger
+
+
+def initLogger(folderName, propagate=False):
+    filePath = '{}/log.txt'.format(folderName)
+    logger = setup_logging(filePath, 'darts', propagate)
+
+    logger.info('Experiment dir: [{}]'.format(folderName))
+
+    return logger
+
+
+def initTrainLogger(logger_file_name, save_path, propagate=False):
+    folder_path = '{}/train'.format(save_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    log_file_path = '{}/{}.txt'.format(folder_path, logger_file_name)
+    logger = setup_logging(log_file_path, logger_file_name, propagate)
+
+    return logger
+
+
+def logDominantQuantizedOp(model, k, logger):
+    top = model.topOps(k=k)
+    logger.info('=============================================')
+    logger.info('Top [{}] quantizations per layer:'.format(k))
+    logger.info('=============================================')
+    for i, layerTop in enumerate(top):
+        message = 'Layer:[{}]  '.format(i)
+        for w, layer in layerTop:
+            message += 'w:[{:.3f}]  bitwidth:{}  act_bitwidth:{}  ||  '.format(w, layer.bitwidth, layer.act_bitwidth)
+
+        logger.info(message)
+
+    logger.info('=============================================')
+
+
+def printModelToFile(model, save_path):
+    filePath = '{}/model.txt'.format(save_path)
+    logger = setup_logging(filePath, 'modelLogger')
+    logger.info('{}'.format(model))
+    logDominantQuantizedOp(model, k=2, logger=logger)
